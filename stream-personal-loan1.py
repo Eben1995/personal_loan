02d0b38fb2c477725b278517a0eb9a35372516dc
Eben1995 <77666669+Eben1995@users.noreply.github.com>
2021-04-14 10:49:49 +0530

    Update and rename stream_personal_loan1.py to stream-personal-loan1.py

--- stream-personal-loan1.py
+++ stream-personal-loan1.py
@@ -3,7 +3,7 @@ from pycaret.classification import load_model, predict_model
 import streamlit as st
 import pandas as pd
 import numpy as np
-model = load_model('bank_loan (1)')
+model = load_model('bank_loan')
 
 
 
